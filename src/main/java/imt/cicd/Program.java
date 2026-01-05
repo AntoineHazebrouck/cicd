ef2024ea883ab42b8146f5684f1fff43db203779
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2026-01-05 10:37:50 +0100

    qsdsqd

--- src/main/java/imt/cicd/Program.java
+++ src/main/java/imt/cicd/Program.java
@@ -1,13 +1,15 @@
 package imt.cicd;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@StyleSheet(Lumo.STYLESHEET)
 @SpringBootApplication
 class Program {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Program.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Program.class, args);
+    }
 }
