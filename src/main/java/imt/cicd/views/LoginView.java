34921f8ad3c42df5de6ab2c29eaa30494fbc9e7b
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2026-01-08 15:31:30 +0100

    title for login

--- src/main/java/imt/cicd/views/LoginView.java
+++ src/main/java/imt/cicd/views/LoginView.java
@@ -2,10 +2,12 @@ package imt.cicd.views;
 
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 
 @Route("login")
+@PageTitle("Login")
 @AnonymousAllowed
 public class LoginView extends VerticalLayout {
 
