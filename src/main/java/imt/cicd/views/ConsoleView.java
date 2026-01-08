1496ba56e088f82f120cc2b7bb11723b14357817
ClementDlre <clement.dlre@gmail.com>
2026-01-08 13:39:34 +0100

    update consoleview

--- src/main/java/imt/cicd/views/ConsoleView.java
+++ src/main/java/imt/cicd/views/ConsoleView.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import imt.cicd.views.components.PipelineStepper;
 import imt.cicd.views.components.PipelinesGrid;
 import jakarta.annotation.security.PermitAll;
 
@@ -15,13 +16,16 @@ import jakarta.annotation.security.PermitAll;
 public class ConsoleView extends Composite<VerticalLayout> {
 
     private final PipelinesGrid pipelines = new PipelinesGrid();
+    private final PipelineStepper stepper = new PipelineStepper();
 
     @Override
     protected VerticalLayout initContent() {
         return new VerticalLayout(
             new H1("Console"),
+                stepper,
             new Button("Refresh", event -> {
                 pipelines.refresh();
+                stepper.reset();
             }),
             pipelines
         );
